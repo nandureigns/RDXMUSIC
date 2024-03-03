f0eebf13a58cd996e968686b04c0b0a79c0db19e
nandureigns <139434750+nandureigns@users.noreply.github.com>
2024-03-03 22:51:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/RDX-RAj/RDXMUSIC",
+    "https://github.com/nandureigns/RDXMUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
